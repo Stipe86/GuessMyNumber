223979b333e230304f15d8343ad57e7a884206f9
Stjepan <stipe.milk@gmail.com>
2023-03-28 22:40:43 +0200

    Function displayMessage() added

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const giveRandomNumber = function () {
   return randomNumb;
 };
 
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
 // let numberToGuess = document.querySelector(".number").value =
 //   giveRandomNumber();
 
@@ -13,22 +16,15 @@ let numberToGuess = document.querySelector(".number").value;
 
 numberToGuess = giveRandomNumber();
 
-console.log("Check numberToGuess: " + numberToGuess);
+// console.log("Check numberToGuess: " + numberToGuess);
 
 let scoreNumber = Number(document.querySelector(".score").textContent);
 
-console.log("check score: " + scoreNumber);
+// console.log("check score: " + scoreNumber);
 
 let highScoreNumber = Number(document.querySelector(".highscore").textContent);
 
-console.log("check highscore: " + highScoreNumber);
-
-// console.log(document.querySelector(".number").value);
-
-// const x = function () {
-//   const numberInput = Number(document.querySelector(".guess").value);
-//   console.log(numberInput, typeof numberInput);
-// };
+// console.log("check highscore: " + highScoreNumber);
 
 document.querySelector(".again").addEventListener("click", function () {
   numberToGuess = giveRandomNumber();
@@ -37,7 +33,7 @@ document.querySelector(".again").addEventListener("click", function () {
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
   document.querySelector(".number").textContent = "?";
-  document.querySelector(".message").textContent = "Start guessing...";
+  displayMessage("Start guessing...");
   scoreNumber = 20;
   document.querySelector(".score").textContent = scoreNumber;
   document.querySelector(".guess").value = "";
@@ -45,33 +41,31 @@ document.querySelector(".again").addEventListener("click", function () {
 
 document.querySelector(".check").addEventListener("click", function () {
   const numberInput = Number(document.querySelector(".guess").value);
-  // console.log(numberInput, typeof numberInput);
 
   if (!numberInput) {
-    document.querySelector(".message").textContent = "⛔ No number";
+    displayMessage("⛔ No number");
   } else if (numberInput < 0 || numberInput > 20) {
-    document.querySelector(".message").textContent =
-      "⛔ Not a number between 1 and 20";
+    displayMessage("⛔ Not a number between 1 and 20");
   } else if (
     scoreNumber === 1 &&
     (numberInput < numberToGuess || numberInput > numberToGuess)
   ) {
-    document.querySelector(".message").textContent = "💀 You lost the game!";
+    displayMessage("💀 You lost the game!");
     scoreNumber--;
     document.querySelector(".score").textContent = scoreNumber;
     document.querySelector("body").style.backgroundColor = "red";
     document.querySelector(".check").disabled = true;
     document.querySelector(".guess").disabled = true;
   } else if (numberInput < numberToGuess) {
-    document.querySelector(".message").textContent = "Too Low!";
+    displayMessage("Too Low!");
     scoreNumber--;
     document.querySelector(".score").textContent = scoreNumber;
   } else if (numberInput > numberToGuess) {
-    document.querySelector(".message").textContent = "Too High!";
+    displayMessage("Too High!");
     scoreNumber--;
     document.querySelector(".score").textContent = scoreNumber;
   } else if (numberInput === numberToGuess) {
-    document.querySelector(".message").textContent = "🎉 Correct number!";
+    displayMessage("🎉 Correct number!");
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = numberToGuess;
     if (highScoreNumber < scoreNumber) {
@@ -81,14 +75,7 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".highscore").textContent = highScoreNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
 
-    // document.querySelector(".check").addEventListener("click", function () {
-
-    // });
     document.querySelector(".check").disabled = true;
     document.querySelector(".guess").disabled = true;
   }
 });
-
-// while (true) {
-//   document.querySelector(".number").value = giveRandomNumber();
-// }
